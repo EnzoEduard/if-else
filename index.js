ff22d384894cb9a1093ea3c76c272d49d6a42868
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-09-05 17:28:44 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,132 @@
+/*
+let condicao1 = true;
+if (condicao1){
+  //  como o valor da condição é true,
+  // o código desse bloco é executado
+  console.log('entrei no if 1!');
+}
+let condicao2 = false;
+if (condicao2){
+  //  como o valor da condição é true,
+  // o código desse bloco não é executado
+  console.log('entrei no if 2!');
+}
+*/
+/*
+function comparacao(numero1,numero2){
+let comparar = numero1 === numero2
+if (comparar){
+   return('os numeros são iguais')
+}
+}
+console.log(
+  recebeDoisNumeros(
+    Number(prompt("digite um numero")),
+    Number(prompt("digite outro numero"))
+  )
+);
+*/
+/*
+function textoENumero(nome,idade){
+  if (idade >= 18){
+    return(nome + ', voce pode dirigir')
+  }
+}
+console.log(
+  textoENumero(
+    prompt("digite seu nome:"),
+    Number(prompt("digite dua idade:"))
+  )
+);
+*/
+/*
+let condicao = false;
+if (condicao){
+  console.log('entrei no if!');
+} else {
+// como o valor da condição é falsa,
+// o código do bloco else será executado
+console.log('entrei no else!')
+}
+*/
+/*
+function comparacao(numero1,numero2){
+  let comparar = numero1 === numero2
+  if (comparar){
+     return('os numeros são iguais')
+  } else {
+    return('os numeros não são iguais')
+  }
+  }
+  console.log(
+    comparacao(
+      Number(prompt("digite um numero")),
+      Number(prompt("digite outro numero"))
+    )
+  );
+  */
+ /*
+  function comparacao(numero1,numero2){
+    if (numero1 === numero2){
+       return('os numeros são iguais')
+    } else if (numero1 > numero2){
+      return('o primeiro é maior que o segundo')
+    } else {
+      return('o primeiro é menor que o segundo')
+    }
+  }
+
+    console.log(
+      comparacao(
+        Number(prompt("digite um numero")),
+        Number(prompt("digite outro numero"))
+      )
+    );
+    */
+   /*
+   let paisDeOrigem
+   switch (paisDeOrigem){
+    case 'Brasil':
+      console.log('brasileiro')
+      break
+      case 'EUA':
+      console.log('norte americano')
+      break
+      case 'Inglaterra':
+      console.log('inglês')
+      break
+      
+      default:
+        console.log('nacionalidade não encontrada')
+        break
+   }
+   */
+  /*
+  let pokemon = prompt('qual é seu pokemon?');
+  switch (pokemon){
+    case 'bulbasauro':
+      console.log('pokemon tipo planta e veneno')
+      break
+      case 'charmander':
+        console.log('pokemon tipo fogo')
+        break
+        case 'squirtle':
+          console.log('pokemon tipo agua')
+          break
+
+          default:
+            console.log('nenhum pokemon escolhido')
+
+  }
+ */
+/*
+function info (ensinoMedio,maiorDeIdade,cursandoAlgo){
+  if(ensinoMedio == "sim" && maiorDeIdade >= 18 && cursandoAlgo == "nao"){
+    return "pode estudar na faculdade";
+  }else {return "nao pode estudar na faculdade";}
+}
+console.log(info(prompt("concluiu o ensino medio?").toLocaleLowerCase(),
+Number(prompt("sua idade?")),
+prompt("esta cursando outra faculdade?").toLocaleLowerCase(),
+));
+*/
